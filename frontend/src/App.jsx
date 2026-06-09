c9142d65c64b1693b2c7c12222f9e47f2a0f08ed
sarthakzenuxcoder <harshgangwar789@gmail.com>
2026-06-09 10:16:14 +0530

    Add React Router and page routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
+import DashboardPage from "./pages/DashboardPage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <div>
-      <LoginPage /> 
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path = "/register" element={<RegisterPage/>} />
+      <Route path="/dashboard" element={<DashboardPage/>} />
+    </Routes>
   );
 }
 
